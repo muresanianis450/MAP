a67554688bd5a31ba4793d4a5a285c5abd4bbc0d
Muresan Ianis-Constantin <ianis.constantin.muresan@stud.ubbcluj.ro>
2025-10-27 09:23:17 +0200

    Update VariableExpression.java

--- src/model/expression/VariableExpression.java
+++ src/model/expression/VariableExpression.java
@@ -1,11 +1,12 @@
 package model.expression;
 import model.value.Value;
 import model.state.SymbolTable;
+import model.exceptions.MyException;
 
 public record VariableExpression(String variableName) implements Expression {
 
     @Override
-    public Value evaluate(SymbolTable symbolTable){
+    public Value evaluate(SymbolTable symbolTable)throws MyException{
         if(!symbolTable.isDefined(variableName)){
             throw new RuntimeException("Variable not defined<Variable_Execution_evaluate>");
         }
