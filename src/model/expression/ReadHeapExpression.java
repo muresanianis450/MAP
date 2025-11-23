4956a3e162072ae234046c057572a82cf659f25f
muresanianis450 <ianis.constantin.muresan@stud.ubbcluj.ro>
2025-11-23 19:07:21 +0200

    Create ReadHeapExpression.java

---
+++ src/model/expression/ReadHeapExpression.java
@@ -0,0 +1,44 @@
+package model.expression;
+
+import exceptions.MyException;
+import model.ADT.Heap.IHeap;
+import model.ADT.Map.IMap;
+import model.value.Value;
+import model.value.RefValue;
+import model.expression.Expression;
+
+public class ReadHeapExpression implements Expression{
+    private final Expression expression;
+
+    public ReadHeapExpression(Expression expression){
+        this.expression = expression;
+    }
+
+    @Override
+    public Value evaluate(IMap<String, Value> symTable, IHeap heap) throws MyException {
+        //evaluate inner expression
+        Value value = expression.evaluate(symTable, heap);
+
+        //check that it is a RefValue
+        if (!(value instanceof RefValue))
+            throw new MyException("ReadHeapExpression: expression is not a RefValue");
+
+        RefValue refValue = (RefValue) value;
+        int address = refValue.getAddress();
+
+        //read from heap
+        if (!heap.isDefined(address))
+            throw new MyException("ReadHeapExpression: address " + address + " is not defined in the heap");
+
+        if(address == -1 )
+            throw new MyException("Cannot read from uninitialized reference.");
+
+        return heap.get(address);
+    }
+
+    @Override
+    public String toString(){
+        return "rH(" + expression.toString() + ")";
+    }
+
+}
