7869ced336fe98c953cfe1bd703e6f2ac8934965
muresanianis450 <ianis.constantin.muresan@stud.ubbcluj.ro>
2025-12-07 16:49:28 +0200

    Update Controller.java

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -8,6 +8,25 @@ import java.util.List;
 import java.util.concurrent.*;
 import java.util.stream.Collectors;
 
+
+/*EXPLICATIONS:
+removeCompletedPrg:
+    -inPrgList.stream() : converts the List<ProgramState> into a stream, which allows
+                          functional-style operations like filter, map , forEach
+    -.filter(ProgramState::isNotCompleted)
+                        : filter keeps only elements that satisfy a predicate
+                          This is equivalent to: .filter( p -> p.isNotCompleted())
+    -.collect(Collectors.toList()) : after filtering, collect gathers the remaining elements back into a List
+                                   Collectors.toList() is a standard way to convert a stream back to a list
+
+oneStepForAllPrg
+    -prgList.stream() : same thing
+    -.map(...)         : map transforms each element of the stream into something else.
+                        Here: each ProgramState p is transformed into a Callable<ProgramState>
+                        The lambda () -> p.oneStep() is the body of the callable. It will execute onStep of the program when called
+                        (Callable<ProgramState>) is just casting it explicitly
+    -.collect(Collectors.toList()) : converts the stream of Callables back into a list, ready to be executed concurrently.
+* */
 public class Controller {
 
     private IRepository repo;
