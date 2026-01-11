b1208fdbe09dbfe1a1e6901a8ecc2e35a96c596c
muresanianis450 <ianis.constantin.muresan@stud.ubbcluj.ro>
2026-01-11 15:08:22 +0200

    Refactoring Project

---
+++ src/gui/MainApp.java
@@ -0,0 +1,26 @@
+package gui;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("JavaFX is working ✅");
+        StackPane root = new StackPane(label);
+
+        Scene scene = new Scene(root, 400, 200);
+
+        stage.setTitle("JavaFX Test");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
